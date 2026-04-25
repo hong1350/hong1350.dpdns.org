2fac35fb449bfd1e055162c897b59c15e0126551
hong1350 <hong1350602@outlook.com>
2026-04-25 22:27:49 +0800

    更新网站内容

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -9,11 +9,11 @@ function homepage() {
     <title>欢迎</title>
   </head>
   <body>
+  <script src="https://keepandroidopen.org/banner.js?lang=zh"></script>
     <h1>什么也没有</h1>
     敬请期待……（正在因为加密问题而左右脑互搏）
     <br>
     现在没有人机验证了（因为没有服务器，验证不了一点。）
-    Cloudflare我[请文明用语]！！！（仪表板又双叒叕登不上了）
   </body>
 </html>
 `;
